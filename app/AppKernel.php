bddac3f2ae889676875cbb5abab561efcced9b53
Stratege takam <Sdanicktakam@yahoo.fr>
2017-06-14 17:45:18 +0100

    generation du  bundle ApiBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 	    new JMS\SerializerBundle\JMSSerializerBundle(),
             new AppBundle\AppBundle(),
+	    new ApiBundle\ApiBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
