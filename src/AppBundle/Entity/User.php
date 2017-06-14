27a699b58fa26c3e59098a4c408564a9f92d8161
Danick Takam <Sdanicktakam@yahoo.fr>
2017-06-14 23:50:37 +0200

    Modificiation de l'entite user et  liaison avec baseuser de fos

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
 
 /**
  * User
@@ -10,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="user")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
  */
-class User
+class User extends  BaseUser
 {
     /**
      * @var int
@@ -35,12 +36,6 @@ class User
      */
     private $lastName;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="email", type="string", length=255)
-     */
-    private $email;
 
     /**
      * @var \DateTime
@@ -122,29 +117,6 @@ class User
         return $this->lastName;
     }
 
-    /**
-     * Set email
-     *
-     * @param string $email
-     *
-     * @return User
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    /**
-     * Get email
-     *
-     * @return string
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
 
     /**
      * Set birthDate
