a08124643e7eb3950428de4006ca0b80cf2e14d4
christiansoh <sohchristian94@gmail.com>
2017-06-14 11:41:57 +0100

    updateUserEntity

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -24,30 +24,65 @@ class User
     /**
      * @var string
      *
-     * @ORM\Column(name="firstName", type="string", length=255)
+     * @ORM\Column(name="firstName", type="string", length=50)
      */
     private $firstName;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="lastName", type="string", length=255)
+     * @ORM\Column(name="lastName", type="string", length=50)
      */
     private $lastName;
 
     /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="birthDay", type="date")
+     */
+    private $birthDay;
+
+    /**
      * @var string
      *
-     * @ORM\Column(name="email", type="string", length=255)
+     * @ORM\Column(name="gender", type="string", length=20)
      */
-    private $email;
+    private $gender;
 
     /**
      * @var \DateTime
      *
-     * @ORM\Column(name="birthDate", type="date")
+     * @ORM\Column(name="lastConnectionDate", type="datetime")
      */
-    private $birthDate;
+    private $lastConnectionDate;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="lastActivity", type="datetime")
+     */
+    private $lastActivity;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="confirmed", type="boolean")
+     */
+    private $confirmed;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="role", type="array")
+     */
+    private $role;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=20)
+     */
+    private $type;
 
     /**
      * @var string
@@ -57,11 +92,25 @@ class User
     private $profession;
 
     /**
+     * @var array
+     *
+     * @ORM\Column(name="phone", type="array")
+     */
+    private $phone;
+
+    /**
      * @var string
      *
-     * @ORM\Column(name="type", type="string", length=255)
+     * @ORM\Column(name="joinReason", type="string", length=255)
      */
-    private $type;
+    private $joinReason;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="isVip", type="boolean")
+     */
+    private $isVip;
 
 
     /**
@@ -123,51 +172,171 @@ class User
     }
 
     /**
-     * Set email
+     * Set birthDay
      *
-     * @param string $email
+     * @param \DateTime $birthDay
      *
      * @return User
      */
-    public function setEmail($email)
+    public function setBirthDay($birthDay)
     {
-        $this->email = $email;
+        $this->birthDay = $birthDay;
 
         return $this;
     }
 
     /**
-     * Get email
-     *
-     * @return string
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * Set birthDate
-     *
-     * @param \DateTime $birthDate
-     *
-     * @return User
-     */
-    public function setBirthDate($birthDate)
-    {
-        $this->birthDate = $birthDate;
-
-        return $this;
-    }
-
-    /**
-     * Get birthDate
+     * Get birthDay
      *
      * @return \DateTime
      */
-    public function getBirthDate()
+    public function getBirthDay()
     {
-        return $this->birthDate;
+        return $this->birthDay;
+    }
+
+    /**
+     * Set gender
+     *
+     * @param string $gender
+     *
+     * @return User
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    /**
+     * Get gender
+     *
+     * @return string
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * Set lastConnectionDate
+     *
+     * @param \DateTime $lastConnectionDate
+     *
+     * @return User
+     */
+    public function setLastConnectionDate($lastConnectionDate)
+    {
+        $this->lastConnectionDate = $lastConnectionDate;
+
+        return $this;
+    }
+
+    /**
+     * Get lastConnectionDate
+     *
+     * @return \DateTime
+     */
+    public function getLastConnectionDate()
+    {
+        return $this->lastConnectionDate;
+    }
+
+    /**
+     * Set lastActivity
+     *
+     * @param \DateTime $lastActivity
+     *
+     * @return User
+     */
+    public function setLastActivity($lastActivity)
+    {
+        $this->lastActivity = $lastActivity;
+
+        return $this;
+    }
+
+    /**
+     * Get lastActivity
+     *
+     * @return \DateTime
+     */
+    public function getLastActivity()
+    {
+        return $this->lastActivity;
+    }
+
+    /**
+     * Set confirmed
+     *
+     * @param boolean $confirmed
+     *
+     * @return User
+     */
+    public function setConfirmed($confirmed)
+    {
+        $this->confirmed = $confirmed;
+
+        return $this;
+    }
+
+    /**
+     * Get confirmed
+     *
+     * @return bool
+     */
+    public function getConfirmed()
+    {
+        return $this->confirmed;
+    }
+
+    /**
+     * Set role
+     *
+     * @param array $role
+     *
+     * @return User
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * Get role
+     *
+     * @return array
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return User
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
     }
 
     /**
@@ -195,27 +364,75 @@ class User
     }
 
     /**
-     * Set type
+     * Set phone
      *
-     * @param string $type
+     * @param array $phone
      *
      * @return User
      */
-    public function setType($type)
+    public function setPhone($phone)
     {
-        $this->type = $type;
+        $this->phone = $phone;
 
         return $this;
     }
 
     /**
-     * Get type
+     * Get phone
+     *
+     * @return array
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set joinReason
+     *
+     * @param string $joinReason
+     *
+     * @return User
+     */
+    public function setJoinReason($joinReason)
+    {
+        $this->joinReason = $joinReason;
+
+        return $this;
+    }
+
+    /**
+     * Get joinReason
      *
      * @return string
      */
-    public function getType()
+    public function getJoinReason()
     {
-        return $this->type;
+        return $this->joinReason;
+    }
+
+    /**
+     * Set isVip
+     *
+     * @param boolean $isVip
+     *
+     * @return User
+     */
+    public function setIsVip($isVip)
+    {
+        $this->isVip = $isVip;
+
+        return $this;
+    }
+
+    /**
+     * Get isVip
+     *
+     * @return bool
+     */
+    public function getIsVip()
+    {
+        return $this->isVip;
     }
 }
 
